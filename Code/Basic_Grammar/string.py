8f451a91a90622cfc767da8250287901bf845612
gdis5251 <841784596@qq.com>
2019-06-22 21:10:17 +0800

    [Gerald][write][2019/6/22] Python syntax practice

---
+++ Code/Basic_Grammar/string.py
@@ -0,0 +1,9 @@
+to_me = """
+Gerald, I trust you can be a great man,
+And you will get a best offer,
+You will invate your family and your friends have a dinner,
+To appreciate there's understand and support.
+Fighting!!!
+"""
+
+print(to_me)
